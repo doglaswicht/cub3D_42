11aa0bfcc3fa64787cc9d1660e7373988b2cfc8f
LaSturn <sturny.cs@gmail.com>
2025-11-16 09:42:10 +0100

    modification collition.c pour que peut gérer des maps avec largeur différente, a vérifier si vraimetn utile

--- src/input/collision.c
+++ src/input/collision.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   collision.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dleite-b <dleite-b@student.42lausanne.c    +#+  +:+       +#+        */
+/*   By: csturny <csturny@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 20:40:31 by dleite-b          #+#    #+#             */
-/*   Updated: 2025/11/15 20:56:12 by dleite-b         ###   ########.fr       */
+/*   Updated: 2025/11/16 09:37:37 by csturny          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
     * verifier si la position (x, y) dans le monde est un mur '1'
     * Convertion des cordenadas reais do jogador para coordenadas interas do mapa
 */
+// attention modification csturny // gère longuer de murs non constante
 int is_wall(const t_world *w, double x, double y)
 {
     int mx = (int)x;
     int my = (int)y;
 
-
-    
-    /*Fora do map -> trate como parede para evitar segfault*/
-if(mx < 0 || my < 0 || my >= w->map_h || mx >= (int)ft_strlen(w->map[my]))
-    return (1);
-
-return(w->map[my][mx] == '1');
+    /* Hors map ou hors ligne = mur */
+    if (mx < 0 || my < 0 || my >= w->map_h)
+        return (1);
+    int line_len = w->map[my] ? (int)ft_strlen(w->map[my]) : 0;
+    if (mx >= line_len)
+        return (1);
+    return (w->map[my][mx] == '1');
 
 }
 
