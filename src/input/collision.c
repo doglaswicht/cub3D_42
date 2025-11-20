12fc4f98d3bf497f2d45db55f3f5341cbfc1d2db
dw <doglasbastos@hotmail.com>
2025-11-20 01:19:59 +0100

    correction norminette colision.c

--- src/input/collision.c
+++ src/input/collision.c
@@ -6,30 +6,31 @@
 /*   By: dleite-b <dleite-b@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 20:40:31 by dleite-b          #+#    #+#             */
-/*   Updated: 2025/11/18 16:47:01 by dleite-b         ###   ########.fr       */
+/*   Updated: 2025/11/20 01:19:12 by dleite-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
 /*
-    * verifier si la position (x, y) dans le monde est un mur '1'
-    * Convertion des cordenadas reais do jogador para coordenadas interas do mapa
+** Retorna 1 se (x, y) é um muro OU está fora do mapa
 */
-int is_wall(const t_world *w, double x, double y)
+int	is_wall(const t_world *w, double x, double y)
 {
-    int mx = (int)x;
-    int my = (int)y;
+	int	mx;
+	int	my;
+	int	line_len;
 
-
-    
-    /*Fora do map -> trate como parede para evitar segfault*/
-if(mx < 0 || my < 0 || my >= w->map_h)
-    return (1);
-int line_len = w->map[my] ? (int)ft_strlen(w->map[my]) : 0; //changer
-if(mx >= line_len)
-    return(1);
-
-return(w->map[my][mx] == '1');
-
+	mx = (int)x;
+	my = (int)y;
+	if (mx < 0 || my < 0 || my >= w->map_h)
+		return (1);
+	if (!w->map[my])
+		return (1);
+	line_len = ft_strlen(w->map[my]);
+	if (mx >= line_len)
+		return (1);
+	if (w->map[my][mx] == '1')
+		return (1);
+	return (0);
 }
-
