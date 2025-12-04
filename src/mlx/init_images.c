b9203c788dfc9126afd4e756ee181ff42f535602
LaSturn <sturny.cs@gmail.com>
2025-12-04 17:45:51 +0100

    modification init_imges.c

--- src/mlx/init_images.c
+++ src/mlx/init_images.c
@@ -6,7 +6,7 @@
 /*   By: csturny <csturny@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 13:16:16 by csturny           #+#    #+#             */
-/*   Updated: 2025/12/02 13:16:17 by csturny          ###   ########.fr       */
+/*   Updated: 2025/12/04 17:44:17 by csturny          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static int	create_frame(t_game *g)
 	g->frame.img = mlx_new_image(g->mlx, WIN_W, WIN_H);
 	if (!g->frame.img)
 		return (1);
+	g->frame.w = WIN_W;
+	g->frame.h = WIN_H;
 	g->frame.addr = mlx_get_data_addr(g->frame.img, &g->frame.bpp,
 			&g->frame.line_len, &g->frame.endian);
 	return (g->frame.addr == NULL);
